508712808dfac1cb1017e73e25e3309d6a4eb5f8
Aniruddha Chatterjee <ruddha.mine@gmail.com>
2021-02-18 22:21:00 +0530

    refactor(jwt): Refactor credential varaibles to work with Lambda

--- user-module/user-controller.js
+++ user-module/user-controller.js
@@ -14,8 +14,8 @@ const lambda = new Lambda({
   apiVersion: "2015-03-31",
   region: "ap-south-1",
   credentials: new Credentials({
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    accessKeyId: process.env.ACCESS_KEY_ID,
+    secretAccessKey: process.env.SECRET_ACCESS_KEY,
   }),
 });
 
