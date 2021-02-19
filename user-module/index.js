90329c1619880be232c0b26e5aaa91ad4ea734c4
Aniruddha Chatterjee <ruddha.mine@gmail.com>
2021-02-19 11:44:57 +0530

    chore(cors): Enable CORS

--- user-module/index.js
+++ user-module/index.js
@@ -2,6 +2,7 @@
 require("dotenv").config();
 
 const express = require("express");
+const cors = require("cors");
 const { logger } = require("./constants");
 const {
   addNewContactUsDocument,
@@ -11,6 +12,7 @@ const {
 const app = express();
 
 // Middlewares required by the app
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
