f86e590f6848ceff68b90138b4ae119fe2dd38a1
Aniruddha Chatterjee <ruddha.mine@gmail.com>
2021-03-07 12:46:01 +0530

    :sparkles: Add custom user functions

--- user-module/user-controller.js
+++ user-module/user-controller.js
@@ -2,8 +2,10 @@ const ContactSchema = require("./schemas/contact-us-schema");
 const UserSchema = require("./schemas/user-schema");
 const { Lambda, Credentials } = require("aws-sdk");
 const mongoose = require("mongoose");
+const argon2 = require("argon2");
 const { google } = require("googleapis");
 const { logger, Errors } = require("./constants");
+const CustomUserSchema = require("./schemas/custom-user-schema");
 const oauth2Client = new google.auth.OAuth2(
   process.env.CLIENT_ID,
   process.env.CLIENT_SECRET,
@@ -163,10 +165,63 @@ const getUser = async (token) => {
   }
 };
 
+const userLogin = async (username, password) => {
+  try {
+    await mongoose.connect(process.env.MONGOOSE_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+      useCreateIndex: true,
+    });
+    const dbData = await CustomUserSchema.findOne({ username: username });
+    if (dbData === null) throw Error("Could not find user in database");
+    if (dbData.password && (await argon2.verify(dbData.password, password))) {
+      const lambdaPromise = lambda
+        .invoke({
+          FunctionName: "jwt-function",
+          Payload: JSON.stringify({
+            operation: "sign",
+            username: username,
+          }),
+        })
+        .promise();
+      const responseData = await lambdaPromise;
+      const responseObject = JSON.parse(responseData.Payload);
+      if (responseObject.errorMessage) throw responseObject.errorMessage;
+      return responseObject;
+    } else throw Error("Could not match hash");
+  } catch (error) {
+    logger.error(error);
+    throw {
+      code: 403,
+      message: "User details are either invalid or not found",
+    };
+  }
+};
+
+const createCustomUser = async (data, token) => {
+  try {
+    if (token !== process.env.ADMIN_TOKEN) throw Error("Invalid admin token");
+    await mongoose.connect(process.env.MONGOOSE_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+      useCreateIndex: true,
+    });
+    data.password = await argon2.hash(data.password);
+    await CustomUserSchema.create(data);
+  } catch (error) {
+    logger.error(error);
+    throw { code: 500, message: "Could not create a new custom user" };
+  }
+};
+
 module.exports = {
   addNewContactUsDocument: addNewContactUsDocument,
   sendContactUsNotificationEmail: sendContactUsNotificationEmail,
   generateLoginUrl: generateLoginUrl,
   setGoogleToken: setToken,
   getUser: getUser,
+  userLogin: userLogin,
+  createCustomUser: createCustomUser,
 };
